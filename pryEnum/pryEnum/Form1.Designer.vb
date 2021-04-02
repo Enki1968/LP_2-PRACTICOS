cf5f4fbf7734fc944e12032eb9bc6e2e2151a7b8
Enki1968 <franco-sosa@hotmail.com>
2021-04-02 20:38:04 -0300

    Primer comit

---
+++ pryEnum/pryEnum/Form1.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnMostrar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnMostrar
+        '
+        Me.btnMostrar.Location = New System.Drawing.Point(53, 36)
+        Me.btnMostrar.Name = "btnMostrar"
+        Me.btnMostrar.Size = New System.Drawing.Size(168, 41)
+        Me.btnMostrar.TabIndex = 0
+        Me.btnMostrar.Text = "Mostrar Valores"
+        Me.btnMostrar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(286, 124)
+        Me.Controls.Add(Me.btnMostrar)
+        Me.Name = "Form1"
+        Me.Text = "Ejemplo de ENUM"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnMostrar As System.Windows.Forms.Button
+
+End Class
---
+++ pryEnum/pryEnum/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+    'Declaración de ENUM
+    Enum EntradaDatos As Integer 'as Integer es opcional
+        NumeroEntero
+        NumeroDecimal
+        Texto
+        FechaHora
+    End Enum
+    Private Sub btnMostrar_Click(sender As System.Object, e As System.EventArgs) Handles btnMostrar.Click
+
+        Dim EA As EntradaDatos = EntradaDatos.FechaHora
+        ' Muestra el valor numerico
+        MessageBox.Show(EA)  ' muestra 3
+        ' Muestra el valor numerico
+        MessageBox.Show(EA.ToString) ' muestra FechaHora
+
+        'General y fixed, muestran el nombre de Enum
+        MessageBox.Show(EA.ToString("F")) 'DateTime
+        'Decimal, muestran el valor de Enum
+        MessageBox.Show(EA.ToString("D")) 'DateTime
+        'General y fixed, muestra ocho digitos hexadecimal
+        MessageBox.Show(EA.ToString("X")) 'DateTime
+
+    End Sub
+End Class
