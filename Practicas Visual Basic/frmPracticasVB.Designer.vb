8d7a5a9181fdb61eca6fb1393df4cc69e5074566
Enki1968 <franco-sosa@hotmail.com>
2021-04-02 21:03:46 -0300

    Segundo commit

---
+++ Practicas Visual Basic/frmPracticasVB.Designer.vb
@@ -0,0 +1,72 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmPracticasVB
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.cmdAceptar = New System.Windows.Forms.Button()
+        Me.cmdSalir = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'cmdAceptar
+        '
+        Me.cmdAceptar.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.cmdAceptar.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdAceptar.Location = New System.Drawing.Point(107, 247)
+        Me.cmdAceptar.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.cmdAceptar.Name = "cmdAceptar"
+        Me.cmdAceptar.Size = New System.Drawing.Size(162, 47)
+        Me.cmdAceptar.TabIndex = 0
+        Me.cmdAceptar.Text = "Aceptar"
+        Me.cmdAceptar.UseVisualStyleBackColor = False
+        '
+        'cmdSalir
+        '
+        Me.cmdSalir.BackColor = System.Drawing.Color.Purple
+        Me.cmdSalir.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdSalir.ForeColor = System.Drawing.Color.White
+        Me.cmdSalir.Location = New System.Drawing.Point(300, 247)
+        Me.cmdSalir.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.cmdSalir.Name = "cmdSalir"
+        Me.cmdSalir.Size = New System.Drawing.Size(162, 47)
+        Me.cmdSalir.TabIndex = 1
+        Me.cmdSalir.Text = "Salir"
+        Me.cmdSalir.UseVisualStyleBackColor = False
+        '
+        'frmPracticasVB
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 17.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(594, 396)
+        Me.Controls.Add(Me.cmdSalir)
+        Me.Controls.Add(Me.cmdAceptar)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "frmPracticasVB"
+        Me.Text = "Practicas Visual Basic"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents cmdAceptar As Button
+    Friend WithEvents cmdSalir As Button
+End Class
---
+++ Practicas Visual Basic/frmPracticasVB.vb
@@ -0,0 +1,3 @@
+﻿Public Class frmPracticasVB
+
+End Class
